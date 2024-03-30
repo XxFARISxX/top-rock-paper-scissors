a2eb3fac07d6e0e61e10be0676c950326bad9f6b
Faris <125892193+XxFARISxX@users.noreply.github.com>
2024-03-30 23:58:22 +0300

    relocate tie, win & lose strings from playRound function to outside it. inside an array.
    also concatenate the variables with their separate
    result strings inside the playRound function.
    the reason for moving the strings outside the
    function, into separate variables was so i can use
    them to count ties/wins/loses.

--- index.js
+++ index.js
@@ -4,47 +4,49 @@ function getComputerChoice() {
     return randomChoice;
 }
 
+const results = ["its a tie!", "You Win!", "You Lose!", "WHAT THE HELL EVEN IS THAT??!!"];
+/*^ this array stores the result strings that will return after playRound
+    function is executed.*/
+
 function playRound(player, computer) {
 //^ the two parameters will get player and computer input.
 
     player = prompt("choice?").toLocaleLowerCase();
-    /*^ this gets player input through a promt. 
-        also turns the player input to lowercase. */
+    /*^ this gets player input through a prompt. 
+        also turns the player input to lowercase.*/
 
     computer = getComputerChoice();
     /*^ this gets computer input through the getComputerChoice()
-        function. */
+        function.*/
 
     if (player === computer) {
-        return ("its a tie!");
+        return (results[0]/*this returns tie string from results array.*/);
     /*^ if both player and computer inputs are the same, this
-      will return the result as a tie. */
+      will return the result as a tie.*/
     } else if (player === "rock" && computer === "scissors") {
       //^ compares player and computer input with "rock" and "scissors".
-        return ("You Win!" + " Rock beats Scissors.");
+        return (results[1]/*this returns win string from results array.*/ + " Rock beats Scissors.");
         /*^ if player input matches with "rock" and computer input matches
             with "scissors", this will return the result. 
-            otherwise keep going. from line 30 to line 43, it contains 
+            otherwise keep going. from line 34 to line 47, it contains 
             repetition of the same code with different input comparisons 
-            and results. */
+            and results.*/
     } else if (player === "paper" && computer === "rock") {
-        return ("You Win!" + " Paper beats Rock.");
+        return (results[1] + " Paper beats Rock.");
     } else if (player === "scissors" && computer === "paper") {
-        return ("You Win!" + " Scissors beat Paper"
+        return (results[1] + " Scissors beat Paper"
                 /*^ "You Win/Lose!" and "blank beats blank." are 
                     separate strings, because "You Win/Lose!" will later
                     be used to count wins and loses. 
-                    thats the idea at least. */);
+                    thats the idea at least.*/);
     } else if (player === "rock" && computer === "paper") {
-        return ("You Lose!" + " Paper beats Rock.");
+        return (results[2]/*this returns lose string from results array.*/ + " Paper beats Rock.");
     } else if (player === "paper" && computer === "scissors") {
-        return ("You Lose!" + " Scissors beats paper.");
+        return (results[2] + " Scissors beats paper.");
     } else if (player === "scissors" && computer === "rock") {
-        return ("You Lose!" + " Rock beats Scissors.");
+        return (results[2] + " Rock beats Scissors.");
     } else {
-        return ("WHAT THE HELL EVEN IS THAT??!!");
-        /*^ if player input is something other then "rock", "paper"
-            or "scissors", this will return. */
+        return (results[3]/*this returns the string from results array when player input is invalid.*/);
     }
 }
 
